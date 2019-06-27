b9cbd30e5c81d277d2c7fe6b036e62467fd5dd79
tcol <tcol@cl6head.lle.rochester.edu>
2019-06-27 10:50:49 -0400

    first commit

---
+++ myConstants.h
@@ -0,0 +1,9 @@
+// Constants:
+
+static const double deg = M_PI / 180.0;
+static const double rad = 180.0/M_PI;
+static const double deg_to_rad = M_PI/180.0;
+static const double rad_to_deg = 180.0/M_PI;
+static const double MYTINY = 1.0e-30;
+static const double MYHUGE = 1.0e+30;
+
